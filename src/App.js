0b4f6c49cdc94079fb8e4f6c4346365d2d0086ce
Saime-shahzad <58229214+Saime-shahzad@users.noreply.github.com>
2023-10-17 16:25:23 +0500

    minor change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           react app created
+           saime shahzad created a app
         </p>
         <a
           className="App-link"
